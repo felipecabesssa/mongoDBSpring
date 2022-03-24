6c9ea230bd1c426f8a55f178937b3ffca4b3a109
felipecabesssa <felipesoares100@hotmail.com>
2022-03-23 21:28:39 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/br/felipesa/workshopmongo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.br.felipesa.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.br.felipesa.workshopmongo.domain.User;
+import com.br.felipesa.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+//      isso limpa nossa coleção la no mongo.		
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
